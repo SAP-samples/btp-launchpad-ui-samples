3dd76563d8dfdad7f233a133473256ca94f72386
Matthias Schmalz <matthias.schmalz@sap.com>
2022-02-05 13:32:20 +0000

    Add reuse component

---
+++ btp-samples-multiple-apps/lib/src/btp/samples/multiple/apps/lib/comp/reuse/controller/Main.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, JSONModel) {
+	"use strict";
+
+	return Controller.extend("btp.samples.multiple.apps.lib.reuse.comp.controller.Main", {
+
+
+	});
+});
